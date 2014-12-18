d674e43c4a531c43e46585267128ff0dcd485333
kranrao <k.ranrao@gmail.com>
2014-12-18 12:27:38 -0800

    Made additional changes to rooks in Nqueens

--- src/solvers.js
+++ src/solvers.js
@@ -54,7 +54,62 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0;
+  var solution = new Board({n:n});
+  var size = n;
+  var colIndex = 0;
+  var finalCol = 0;
+  var finalRow = 0;
+  // set variable for finalrow
+
+  var createRook = function(column){
+
+    if(colIndex === size){
+      solutionCount++;
+      finalCol++;
+      if(finalCol - 1 && finalRow === size - 1){
+        return solutionCount;
+      } else if(finalCol > size - 1){
+        finalCol = 0;
+        finalRow++;
+        solution = new Board({n:n});
+        createRook(finalCol);
+      } else {
+        solution = new Board({n:n});
+        createRook(finalCol);
+      }
+
+      alert(solutionCount);
+
+      // above states a board has been created
+      // solutionCount gets incremented
+      //
+      // if our new finalrow finalcol have not reached
+        // reset colIndex to 0
+        // run new board
+        // run create rook on this new board at
+        // with a new starting point instead of row[0], column[0]
+      // else
+        // break
+    }
+
+    for(var i = 0; i < size; i++){
+      solution.get(i)[colIndex] = 1;
+
+      if(!solution.hasRowConflictAt(i) && !solution.hasColConflictAt(colIndex)){
+        solution.get(i)[colIndex] = 1;
+      } else {
+        solution.get(i)[colIndex] = 0;
+      }
+      colIndex++;
+      createRook(colIndex);
+    }
+
+
+  }
+
+
+  createRook(colIndex);
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
